1aff3d251f26fa2bd9d4177b2fd8f98249fea61e
Pedro Carvalho <pedrofrancocarvalho@gmail.com>
2020-07-20 14:07:58 -0300

    upload lesson 10 incomplete

---
+++ cap01/aula10/js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", start);
+
+nameList = ["Paulo", "Liana", "Ana", "Pedro", "Zoe", "Dinah"];
+
+function start() {
+  console.log("Totalmente Carregado");
+  listGenarator(nameList);
+}
+
+function listGenarator(list) {
+  for (i = 0; i < list.lenght; i++) {}
+}
