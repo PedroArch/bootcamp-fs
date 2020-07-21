0726e61a93438f4b74171247688364ec0f26c3e2
Pedro Carvalho <pedrofrancocarvalho@gmail.com>
2020-07-20 22:07:45 -0300

    update lesson 10

--- cap01/aula10/js/script.js
+++ cap01/aula10/js/script.js
@@ -3,10 +3,41 @@ window.addEventListener("load", start);
 nameList = ["Paulo", "Liana", "Ana", "Pedro", "Zoe", "Dinah"];
 
 function start() {
-  console.log("Totalmente Carregado");
-  listGenarator(nameList);
+  var form = document.querySelector("form");
+  form.addEventListener("submit", preventSubmit);
+
+  var textName = document.querySelector("#textName");
+  textNameHandle(textName);
+  render();
 }
 
-function listGenarator(list) {
-  for (i = 0; i < list.lenght; i++) {}
+function preventSubmit(event) {
+  event.preventDefault();
+}
+
+function textNameHandle(textName) {
+  function typedName(event) {
+    if (event.key === "Enter") {
+      var nameTyped = event.target.value;
+      nameList.push(nameTyped);
+      render();
+    }
+  }
+
+  textName.focus();
+  textName.addEventListener("keyup", typedName);
+}
+
+function render() {
+  var names = document.querySelector("#names");
+  names.textContent = "";
+  var ul = document.createElement("ul");
+  names.appendChild(ul);
+  for (i = 0; i < nameList.length; i++) {
+    var currentName = nameList[i];
+    var li = document.createElement("li");
+    li.textContent = currentName;
+    li.classList.add("clickable");
+    ul.appendChild(li);
+  }
 }
