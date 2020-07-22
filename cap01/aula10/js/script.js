00449295b0f66e8be8e81446b27a316bf20bd373
Pedro Carvalho <pedrofrancocarvalho@gmail.com>
2020-07-22 00:07:54 -0300

    upload lesson 10 completed

--- cap01/aula10/js/script.js
+++ cap01/aula10/js/script.js
@@ -1,15 +1,15 @@
 window.addEventListener('load', start);
 
 nameList = ['Paulo', 'Liana', 'Ana', 'Pedro', 'Zoe', 'Dinah'];
+editingMode = false;
+currentIndex = null;
 
 function start() {
   var form = document.querySelector('form');
   form.addEventListener('submit', preventSubmit);
 
   var textName = document.querySelector('#textName');
-  var buttons = document.querySelectorAll('.deleteButton');
   textNameHandle(textName);
-  buttonHandle(buttons);
   render();
 }
 
@@ -17,15 +17,57 @@ function preventSubmit(event) {
   event.preventDefault();
 }
 
+function updateName(newName) {
+  nameList[currentIndex] = newName;
+  render();
+}
+
 function textNameHandle(textName) {
   function typedName(event) {
-    if (event.key === 'Enter') {
-      var nameTyped = event.target.value;
-      nameList.push(nameTyped);
-      render();
+    if (event.key === 'Enter' && event.target.value.trim() !== '') {
+      if (editingMode) {
+        updateName(event.target.value);
+      } else {
+        var nameTyped = event.target.value;
+        nameList.push(nameTyped);
+        render();
+      }
+      editingMode = false;
     }
   }
   textName.addEventListener('keyup', typedName);
+}
+
+function createButton(index) {
+  function deleteName() {
+    nameList.splice(index, 1);
+    render();
+  }
+  var button = document.createElement('button');
+  button.textContent = 'x';
+  button.classList.add('deleteButton', 'clickable');
+
+  button.addEventListener('click', deleteName);
+
+  return button;
+}
+
+function createSpan(currentName, index) {
+  function spanEdit() {
+    editingMode = true;
+    textName = document.querySelector('#textName');
+    textName.value = currentName;
+    textName.focus();
+    currentIndex = index;
+  }
+
+  var span = document.createElement('span');
+  span.textContent = currentName;
+  span.classList.add('clickable');
+
+  span.addEventListener('click', spanEdit);
+
+  return span;
 }
 
 function render() {
@@ -33,15 +75,14 @@ function render() {
   names.textContent = '';
   var ul = document.createElement('ul');
   names.appendChild(ul);
+
   for (i = 0; i < nameList.length; i++) {
     var currentName = nameList[i];
+
     var li = document.createElement('li');
-    var span = document.createElement('span');
-    span.textContent = currentName;
-    var button = document.createElement('button');
-    button.textContent = 'x';
-    button.classList.add('deleteButton', 'clickable');
-    li.classList.add('clickable');
+    var span = createSpan(currentName, i);
+    var button = createButton(i);
+
     li.appendChild(button);
     li.appendChild(span);
     ul.appendChild(li);
@@ -50,11 +91,4 @@ function render() {
     textName.value = '';
     textName.focus();
   }
-}
-
-function buttonHandle(buttons) {
-  function deleteHandle(event) {
-    console.log(event);
-  }
-  console.log(buttons);
 }
