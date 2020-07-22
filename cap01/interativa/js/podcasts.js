dd0ac0a5b9af2a183f8b3e12a58eb06766cfa3d8
Pedro Carvalho <pedrofrancocarvalho@gmail.com>
2020-07-22 04:01:53 -0300

    upload projects interativa and desafio

---
+++ cap01/interativa/js/podcasts.js
@@ -0,0 +1,67 @@
+var realPodcast = [
+  {
+    id: "88.5",
+    title: "Flow Podcast",
+    description:
+      "Bate-papo sem papas na lingua com os mais diferentes personalidades",
+    img: "flow.jpg",
+  },
+  {
+    id: "90.2",
+    title: "Jogabilida.de",
+    description:
+      "Podcast de noticias do mundo dos games, além de podcast temáticos",
+    img: "jogabilidade.jpg",
+  },
+  {
+    id: "92.3",
+    title: "Motherchip",
+    description: "Bate-papo sobre games mais focado na industria e indies",
+    img: "motherchip.jpg",
+  },
+  {
+    id: "93.9",
+    title: "Nautilus",
+    description: "Podcast de games que focado nos jogos indies e devs",
+    img: "nautilus.jpg",
+  },
+  {
+    id: "95.1",
+    title: "Nerdcast",
+    description: "Pai dos podcasts do Brasil, um olhar nerd sobre cultura pop",
+    img: "nercast.jpg",
+  },
+  {
+    id: "98.8",
+    title: "Poucas",
+    description:
+      "Papo sincero e sem papas na lingua com convidados apresentado por Caue Moura",
+    img: "poucas.jpg",
+  },
+  {
+    id: "101.1",
+    title: "Projeto Humanos",
+    description:
+      "Serial brasileiro, um podcast sobre crimes não ou suprendentemente resolvidos",
+    img: "projetohumanos.jpg",
+  },
+  {
+    id: "103.0",
+    title: "Sci-cast",
+    description: "Podcast sobre ciências, porque aprender pode ser divertido",
+    img: "scicast.jpg",
+  },
+  {
+    id: "105.3",
+    title: "Serial",
+    description: "[Inglês] Mais famoso e premiado podcast criminal do mundo.",
+    img: "serial.jpg",
+  },
+  {
+    id: "107.9",
+    title: "Tecnocast",
+    description:
+      "Podcast sobre tecnologias novas que o mercado vem apresentando",
+    img: "tecnocast.jpg",
+  },
+];
