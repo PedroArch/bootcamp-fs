3c245fdd82b8a383433a2ecb870594e20be30ca3
Pedro Carvalho <pedrofrancocarvalho@gmail.com>
2020-08-05 00:06:37 -0300

    update lesson 10;

--- cap02/aula09/my-bank/routes/accounts.js
+++ cap02/aula09/my-bank/routes/accounts.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { promises as fs } from "fs";
+import { promises as fs, write } from "fs";
+import { send } from "process";
 
 const { readFile, writeFile } = fs;
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
   try {
     let account = req.body;
     let data = JSON.parse(await readFile("accounts.json"));
@@ -16,25 +17,23 @@ router.post("/", async (req, res) => {
 
     delete data.nextId;
 
-    console.log(data);
-
     res.send(data);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile("accounts.json"));
     delete data.nextId;
     res.send(data);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile("accounts.json"));
     const account = data.accounts.find(
@@ -42,8 +41,56 @@ router.get("/:id", async (req, res) => {
     );
     res.send(account);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile("accounts.json"));
+    data.accounts = data.accounts.filter(
+      (account) => account.id !== parseInt(req.params.id)
+    );
+    res.end();
+    await writeFile("accounts.json", JSON.stringify(data, null, 2));
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/", async (req, res, next) => {
+  try {
+    const account = req.body;
+    const data = JSON.parse(await readFile("accounts.json"));
+    const index = data.accounts.findIndex((acc) => acc.id === account.id);
+
+    data.accounts[index] = account;
+
+    await writeFile("accounts.json", JSON.stringify(data, null, 2));
+    res.send(account);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.patch("/updateBalance", async (req, res, next) => {
+  try {
+    const account = req.body;
+    const data = JSON.parse(await readFile("accounts.json"));
+    const index = data.accounts.findIndex((acc) => acc.id === account.id);
+
+    data.accounts[index].balance = account.balance;
+
+    await writeFile("accounts.json", JSON.stringify(data, null, 2));
+    res.send(data.accounts[index]);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => {
+  console.log(err);
+  res.status(400).send({ error: err.message });
 });
 
 export default router;
